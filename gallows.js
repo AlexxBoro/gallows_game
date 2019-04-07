eb8a6a628c41478ad6b2147869683d18f3441a56
Aleksandra Borowska <aleks.borowska91@gmail.com>
2019-04-07 11:16:17 +0200

    mobile version

--- gallows.js
+++ gallows.js
@@ -1,10 +1,11 @@
-let guesses = ["no money no love", "no women no cry", "carpe diem"]
+let guesses = ["no money no love", "no women no cry", "carpe diem", "no pain no gain", "i never lose either i win or i learn", "who dares wins"];
 
-function getGuess(){
-    let minimum = 0;
-    let maximum = guesses.length-1;
-    let randomnumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-    return guesses[randomnumber];
+function getGuess() {
+  let minimum = 0;
+  let maximum = guesses.length - 1;
+  let randomnumber =
+    Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+  return guesses[randomnumber];
 }
 
 let guess = getGuess();
@@ -17,19 +18,18 @@ let wrongAnswers = 0;
 let yesSound = new Audio("yes.wav");
 let noSound = new Audio("no.wav");
 
-
 let guess1 = "";
 
-for(let i = 0; i < guessLength; i++){
-    if(guess.charAt(i)==" "){
-        guess1 = guess1 + " ";
-    } else {
-        guess1 = guess1 + "-";
-    } 
+for (let i = 0; i < guessLength; i++) {
+  if (guess.charAt(i) == " ") {
+    guess1 = guess1 + " ";
+  } else {
+    guess1 = guess1 + "-";
+  }
 }
 
-function typeEntry(){
-    document.getElementById("input").innerHTML = guess1;
+function typeEntry() {
+  document.getElementById("input").innerHTML = guess1;
 }
 
 window.onload = start;
@@ -63,64 +63,72 @@ letters[23] = "X";
 letters[24] = "Y";
 letters[25] = "Z";
 
+function start() {
+  let alphabetContent = "";
 
-function start(){
-    let alphabetContent = "";
+  for (let i = 0; i <= 25; i++) {
+    let singleLetter = "let" + i;
+    alphabetContent =
+      alphabetContent +
+      '<div class="letter" onclick="checkLetter(' +
+      i +
+      ')" id="' +
+      singleLetter +
+      '">' +
+      letters[i] +
+      "</div>";
+  }
 
-    for (let i = 0; i <= 25; i++){
-        let singleLetter = "let" + i;
-        alphabetContent = alphabetContent + '<div class="letter" onclick="checkLetter('+i+')" id="' + singleLetter + '">' + letters[i] + '</div>'
+  document.getElementById("alphabet").innerHTML = alphabetContent;
+
+  typeEntry();
+}
+
+String.prototype.insertChar = function(place, char) {
+  if (place > this.length - 1) return this.toString();
+  else return this.substr(0, place) + char + this.substr(place + 1);
+};
+
+function checkLetter(num) {
+  let correctGuess = false;
+
+  for (let i = 0; i < guessLength; i++) {
+    if (guess.charAt(i) == letters[num]) {
+      guess1 = guess1.insertChar(i, letters[num]);
+      correctGuess = true;
     }
-
-    document.getElementById("alphabet").innerHTML = alphabetContent;
+  }
+  if (correctGuess == true) {
+    yesSound.play();
+    let singleLetter = "let" + num;
+    document.getElementById(singleLetter).style.background = "#003300";
+    document.getElementById(singleLetter).style.color = "#00C000";
+    document.getElementById(singleLetter).style.border = "3px solid #00C000";
+    document.getElementById(singleLetter).style.cursor = "default";
 
     typeEntry();
-}
+  } else {
+    noSound.play();
+    let singleLetter = "let" + num;
+    document.getElementById(singleLetter).style.background = "#330000";
+    document.getElementById(singleLetter).style.color = "#C00000";
+    document.getElementById(singleLetter).style.border = "3px solid #C00000";
+    document.getElementById(singleLetter).style.cursor = "default";
+    document.getElementById(singleLetter).setAttribute("onclick", ";");
 
-String.prototype.insertChar = function(place, char){
-    if (place > this.length - 1) return this.toString();
-    else return this.substr(0, place) + char + this.substr(place+1);
-}
-
-function checkLetter(num){
-
-    let correctGuess = false;
-
-    for(let i = 0; i<guessLength;i++){
-        if (guess.charAt(i) == letters[num]){
-            guess1 = guess1.insertChar(i,letters[num]);
-            correctGuess = true;
-        }
-    }    
-        if(correctGuess == true){
-            yesSound.play();
-            let singleLetter = "let" + num;
-            document.getElementById(singleLetter).style.background = "#003300";
-            document.getElementById(singleLetter).style.color = "#00C000";
-            document.getElementById(singleLetter).style.border = "3px solid #00C000";
-            document.getElementById(singleLetter).style.cursor = "default";
-
-            typeEntry();
-        }
-        else {
-            noSound.play();
-            let singleLetter = "let" + num;
-            document.getElementById(singleLetter).style.background = "#330000";
-            document.getElementById(singleLetter).style.color = "#C00000";
-            document.getElementById(singleLetter).style.border = "3px solid #C00000";
-            document.getElementById(singleLetter).style.cursor = "default";
-            document.getElementById(singleLetter).setAttribute("onclick", ";");
-
-
-            wrongAnswers++;
-            let pic = "img/s" + wrongAnswers + ".jpg";
-            document.getElementById("gallows").innerHTML = '<img src="'+ pic +'" alt="" />';
-        }
-    if (guess == guess1){
-        document.getElementById("alphabet").innerHTML = 'Success!' + '<br /><br /><span class="reset" onclick="location.reload()">AGAIN?</span>';
-    }    
-    if (wrongAnswers >= 9){
-        document.getElementById("alphabet").innerHTML = 'Fail :-(' + '<br /><br /><span class="reset" onclick="location.reload()">AGAIN?</span>';
-    }
-
+    wrongAnswers++;
+    let pic = "img/s" + wrongAnswers + ".jpg";
+    document.getElementById("gallows").innerHTML =
+      '<img src="' + pic + '" alt="" />';
+  }
+  if (guess == guess1) {
+    document.getElementById("alphabet").innerHTML =
+      "Success!" +
+      '<br /><br /><span class="reset" onclick="location.reload()">AGAIN?</span>';
+  }
+  if (wrongAnswers >= 9) {
+    document.getElementById("alphabet").innerHTML =
+      "Fail :-(" +
+      '<br /><br /><span class="reset" onclick="location.reload()">AGAIN?</span>';
+  }
 }
